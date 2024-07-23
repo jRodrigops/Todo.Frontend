bed9494cab558df94c77ddbc24d79a22fd5d31d0
Rodrigo <rodrigoportela664@gmail.com>
2024-07-22 22:18:46 -0300

    Finalizada função para dar check em cada task

--- src/components/tasks.tsx
+++ src/components/tasks.tsx
@@ -1,31 +1,38 @@
-import { Circle, Trash2 } from "lucide-react";
+import { Check, Circle, CircleCheck, Trash2 } from "lucide-react";
+import { Task } from "../App";
 
-export interface Task{
+export interface TaskProps{
     id:number 
     text: string
     isChecked: boolean
 }
 
-interface TasksProps {
-    tasks: Task[];
-    onRemoveTask: (id: number) => void;
-}
-  
 
-export function Tasks({tasks, onRemoveTask} : TasksProps){
+interface TasksProps {
+    tasks: TaskProps[];
+    onRemoveTask: (id: number) => void;
+    onChangeTask: (updateTask : TaskProps) => void
+}
+
+
+  
+export function Tasks({tasks, onRemoveTask, onChangeTask} : TasksProps){
+    
 
     return(
         <div className="space-y-3">
             {tasks.map((task) => ( 
                 <div key={task.id} className="w-[750px] h-16 px-4 flex items-center gap-3 bg-gray-500 rounded-lg ">
-                <button className="text-blue">
-                 <Circle type="checkbox" />
+
+                <button onClick={() => onChangeTask({...task, isChecked: !task.isChecked})} className="text-blue">
+                    {task.isChecked ? <Check className="text-gray-100  rounded-full bg-purple-dark "/> 
+                    
+                    : <Circle className="text-blue"/>}
                 </button>
 
-                <span className="bg-gray-500 text-gray-100 outline-none" >
-                    {task.text}
+                <span className="bg-gray-500 text-gray-100 outline-none " >
+                    {task.text }
                 </span>
-
 
                 <button className="text-gray-300 hover:text-red-500 ml-auto" onClick={() => onRemoveTask(task.id)}>
                 <Trash2 className="size-5 "/>
